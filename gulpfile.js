91bf3239548269082ec3f20fad637a3a36621419
Diyan Tonchev <diyantonchev@gmail.com>
2017-05-23 14:38:08 +0300

    added gulpfile.js and some tasks

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const shell = require('gulp-shell')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+const rename = require('gulp-rename')
+
+gulp.task('run-db', shell.task([
+  'mongod --dbpath %cd%\\..\\db\\MongoDB\\data'
+]))
+
+gulp.task('run-server', shell.task([
+  'node index.js'
+]))
+
+gulp.task('minify-css', () => {
+  return gulp.src('public/content/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('public/content/styles'))
+})
