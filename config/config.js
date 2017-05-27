1f2321f8ef0573b222101dd02d73dce007121bb5
unknown <diyantonchev@gmail.com>
2017-05-27 18:00:13 +0300

    done some stuff for development env

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/shop-stop',
-  port: process.env.PORT || 8888
+  port: process.env.PORT || 8080
 }
 
 module.exports = config
